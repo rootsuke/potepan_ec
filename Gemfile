436f705860fa334b6490ac1ecd8395df68d614f8
root_suke <rootsuke@gmail.com>
2018-10-22 00:51:23 +0900

    controller specを追加、リファクタリング

--- Gemfile
+++ Gemfile
@@ -50,6 +50,11 @@ group :development, :test do
   gem 'annotate'
 end
 
+group :test do
+  gem 'rails-controller-testing'
+  gem 'capybara'
+end
+
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
   gem 'web-console', '>= 3.3.0'
--- spec/controllers/potepan/products_controller_spec.rb
+++ spec/controllers/potepan/products_controller_spec.rb
@@ -2,11 +2,26 @@ require 'rails_helper'
 
 RSpec.describe Potepan::ProductsController, type: :controller do
   describe "#show" do
-    let(:product) { FactoryBot.create(:product) }
+    let(:product) { create(:product) }
+
+    before do
+      get :show, params: { id: product.id }
+    end
 
     it "responds successfully" do
-      get :show, params: { id: product.id }
       expect(response).to be_successful
+    end
+
+    it "returns a 200 response" do
+      expect(response).to have_http_status "200"
+    end
+
+    it "assigns @product" do
+      expect(assigns(:product)).to eq product
+    end
+
+    it "render show page" do
+      expect(response).to render_template "potepan/products/show"
     end
   end
 end
