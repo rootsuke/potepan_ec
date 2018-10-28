ed9d54164e149cf2ec30097386703dbba634ab22
root_suke <rootsuke@gmail.com>
2018-10-29 01:06:02 +0900

    画面遷移を検証するfeature specを追加

--- spec/features/potepan/potepan_products_spec.rb
+++ spec/features/potepan/potepan_products_spec.rb
@@ -1,10 +1,14 @@
 require 'rails_helper'
 
 RSpec.feature "Potepan::Products", type: :feature do
-  scenario "visit show_page" do
+  scenario "visit index_page and show_page" do
     product = create(:product)
 
+    visit potepan_root_path
+    click_link 'Home'
+    expect(current_path).to eq potepan_index_path
     visit potepan_product_path(product.id)
+    expect(current_path).to eq potepan_product_path(product.id)
     expect(page).to have_content(product.name)
     expect(page).to have_content(product.description)
     expect(page).to have_content(product.display_price)
