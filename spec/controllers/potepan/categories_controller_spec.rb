f96fbbe9a1625a7ab543bbe0a627eb71e290a9c0
root_suke <rootsuke@gmail.com>
2018-11-02 00:47:22 +0900

    コントローラースペックを追加

--- spec/controllers/potepan/categories_controller_spec.rb
+++ spec/controllers/potepan/categories_controller_spec.rb
@@ -1,12 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Potepan::CategoriesController, type: :controller do
-
   describe "GET #show" do
+    let(:taxon) { create(:taxon) }
+
+    before do
+      get :show, params: { id: taxon.id }
+    end
+
     it "returns http success" do
-      get :show
       expect(response).to have_http_status(:success)
     end
-  end
 
+    it "returns 200 response" do
+      expect(response).to have_http_status "200"
+    end
+
+    it "assigns @taxon" do
+      expect(assigns(:taxon)).to eq taxon
+    end
+
+    it "render show_page" do
+      expect(response).to render_template "potepan/categories/show"
+    end
+  end
 end
