94741fb9a5da819bb760a5a5f1a3b39add38a579
root_suke <rootsuke@gmail.com>
2018-11-02 22:37:07 +0900

    feature specのscenarioを分割、ページタイトルの検証を追加

--- spec/features/potepan/potepan_products_spec.rb
+++ spec/features/potepan/potepan_products_spec.rb
@@ -1,16 +1,21 @@
 require 'rails_helper'
 
 RSpec.feature "Potepan::Products", type: :feature do
-  scenario "visit index_page and show_page" do
-    product = create(:product)
+  given(:product) { create(:product) }
 
+  scenario "render index_page" do
     visit potepan_root_path
     click_link 'Home'
-    expect(current_path).to eq potepan_index_path
+    expect(page).to have_title("BIGBAG Store")
+    expect(current_path).to eq(potepan_index_path)
+  end
+
+  scenario "render show_page" do
     visit potepan_product_path(product.id)
-    expect(current_path).to eq potepan_product_path(product.id)
     expect(page).to have_content(product.name)
     expect(page).to have_content(product.description)
     expect(page).to have_content(product.display_price)
+    expect(page).to have_title("#{product.name} | BIGBAG Store")
+    expect(current_path).to eq potepan_product_path(product.id)
   end
 end
