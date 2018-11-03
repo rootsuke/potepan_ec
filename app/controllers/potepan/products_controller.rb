e6de2ea96675504ee2afcb7f32d48910f369ddd1
root_suke <rootsuke@gmail.com>
2018-11-03 19:55:51 +0900

    商品ページに表示する商品の詳細内容を追加

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,5 +1,7 @@
 class Potepan::ProductsController < ApplicationController
   def show
     @product = Spree::Product.find(params[:id])
+    @properties = @product.product_properties
+    @shipping_methods = @product.shipping_category.shipping_methods
   end
 end
