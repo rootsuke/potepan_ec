fb3501f156da807e868f6deae043a66ec74c5f87
root_suke <rootsuke@gmail.com>
2018-11-08 00:45:06 +0900

    bulletの導入、N+1問題への対処

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,5 +1,6 @@
 class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:id])
+    @products = @taxon.products.all.includes(master: :images)
   end
 end
