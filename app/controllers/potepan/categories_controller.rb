2670517c06c1e10adf0adcd048482c1aba3dbd7a
root_suke <rootsuke@gmail.com>
2018-11-10 15:10:44 +0900

    カテゴリーツリーを仮実装

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,6 +1,7 @@
 class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:id])
+    @taxonomies = Spree::Taxonomy.includes(root: :children)
     @products = @taxon.products.all.includes(master: :images)
   end
 end
