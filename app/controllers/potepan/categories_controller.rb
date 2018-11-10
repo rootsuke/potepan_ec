4d4d961b921e8e9dad4abf5d390a8dfa34d95b71
root_suke <rootsuke@gmail.com>
2018-11-11 02:35:44 +0900

    grid、list切替えを仮実装

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -3,5 +3,6 @@ class Potepan::CategoriesController < ApplicationController
     @taxon = Spree::Taxon.find(params[:id])
     @taxonomies = Spree::Taxonomy.includes(root: :children)
     @products = Spree::Product.in_taxon(@taxon).includes(master: [:images, :default_price])
+    @view_type = params[:view_type] ||= "grid"
   end
 end
