ef9f667f94ffaae9d3f874f77d643ad4b271d6e6
root_suke <rootsuke@gmail.com>
2018-11-24 02:37:38 +0900

    関連商品の表示を仮実装

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -3,5 +3,7 @@ class Potepan::ProductsController < ApplicationController
     @product = Spree::Product.find(params[:id])
     @properties = @product.product_properties.includes(:property)
     @shipping_methods = @product.shipping_category.shipping_methods
+    # TODO @productを@related_productsに含めないようにする
+    @related_products = Spree::Product.includes(:classifications, master: [:images, :default_price]).where("spree_products_taxons.taxon_id": @product.taxons).limit(4)
   end
 end
