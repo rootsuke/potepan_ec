b07a35aa0b5a3b917bd243792bd26c7f1eba8aae
root_suke <rootsuke@gmail.com>
2018-11-24 15:33:18 +0900

    現在表示している商品が関連商品一覧に含まれないように修正

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -4,6 +4,9 @@ class Potepan::ProductsController < ApplicationController
     @properties = @product.product_properties.includes(:property)
     @shipping_methods = @product.shipping_category.shipping_methods
     # TODO @productを@related_productsに含めないようにする
-    @related_products = Spree::Product.includes(:classifications, master: [:images, :default_price]).where("spree_products_taxons.taxon_id": @product.taxons).limit(4)
+    @related_products = Spree::Product.
+      includes(:classifications, master: [:images, :default_price]).
+      where("spree_products_taxons.taxon_id": @product.taxons).
+      where.not(id: @product.id).limit(4)
   end
 end
