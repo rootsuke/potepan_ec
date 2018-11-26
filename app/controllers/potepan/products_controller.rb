ba31b1f744a0551b546f35c7055fdc737b1b8084
root_suke <rootsuke@gmail.com>
2018-11-26 22:16:36 +0900

    scopeを修正

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -3,6 +3,6 @@ class Potepan::ProductsController < ApplicationController
     @product = Spree::Product.find(params[:id])
     @properties = @product.product_properties.includes(:property)
     @shipping_methods = @product.shipping_category.shipping_methods
-    @related_products = Spree::Product.related_products_of(@product)
+    @related_products = Spree::Product.related_products_of(@product).limit(4)
   end
 end
