b2c11ba76d5e8abb049416e9ae36df08e4f9e3f1
root_suke <rootsuke@gmail.com>
2018-11-26 22:18:53 +0900

    関連商品が５つ以上ある場合の検証を追加

--- spec/controllers/potepan/products_controller_spec.rb
+++ spec/controllers/potepan/products_controller_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Potepan::ProductsController, type: :controller do
   describe "#show" do
-    let(:product)           { create(:product, taxons: [taxon]) }
+    let(:product)           { create(:product, taxons: [related_taxon]) }
     let(:property)          { create(:property) }
     let!(:product_property) { create(:product_property, value: "red", product: product, property: property) }
     let!(:shipping_method)  { create(:shipping_method) }
-    let(:related_products)  { create_list(:product, 4, taxons: [taxon]) }
-    let(:taxon)             { create(:taxon) }
+
+    let(:taxonomy)      { create(:taxonomy) }
+    let(:related_taxon) { create(:taxon, parent: taxonomy.root, taxonomy: taxonomy) }
 
     before do
       get :show, params: { id: product.id }
@@ -19,6 +20,10 @@ RSpec.describe Potepan::ProductsController, type: :controller do
 
     it "returns a 200 response" do
       expect(response).to have_http_status "200"
+    end
+
+    it "render show page" do
+      expect(response).to render_template :show
     end
 
     it "assigns @product" do
@@ -33,12 +38,36 @@ RSpec.describe Potepan::ProductsController, type: :controller do
       expect(assigns(:shipping_methods)).to eq [shipping_method]
     end
 
-    it "assigns @related_products" do
-      expect(assigns(:related_products)).to eq related_products
-    end
+    describe "Related_products" do
+      let!(:related_products)   { create_list(:product, 4, taxons: [related_taxon]) }
+      let!(:not_related_product) { create(:product,         taxons: [not_related_taxon]) }
+      let(:not_related_taxon)   { create(:taxon, parent: taxonomy.root, taxonomy: taxonomy) }
 
-    it "render show page" do
-      expect(response).to render_template :show
+      it "does not include not_related_product" do
+        expect(assigns(:related_products)).not_to contain_exactly not_related_product
+      end
+
+      context "related_taxon has 4 products" do
+        it "assigns @related_products" do
+          expect(assigns(:related_products)).to eq related_products
+        end
+
+        it "the number of related_products is 4" do
+          expect(assigns(:related_products).count).to eq 4
+        end
+      end
+
+      context "related_taxon has 5 products" do
+        let!(:related_products_5) { create(:product, taxon: [related_taxon]) }
+
+        it "assigns @related_products" do
+          expect(assigns(:related_products)).to eq related_products
+        end
+
+        it "the number of related_products is 4" do
+          expect(assigns(:related_products).count).to eq 4
+        end
+      end
     end
   end
 end
--- spec/features/potepan/products_spec.rb
+++ spec/features/potepan/products_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
 RSpec.feature "Visiting Product Page", type: :feature do
-  given(:product)              { create(:product, taxons: [related_taxon]) }
+  given(:product)              { create(:product,         taxons: [related_taxon]) }
+  given!(:related_products)    { create_list(:product, 4, taxons: [related_taxon]) }
+  given!(:related_product_5)   { create(:product,         taxons: [related_taxon]) }
+  given!(:not_related_product) { create(:product,         taxons: [not_related_taxon]) }
   given(:property)             { create(:property) }
   given!(:product_property)    { create(:product_property, value: "red", product: product, property: property) }
   given!(:shipping_method)     { create(:shipping_method) }
-  given!(:related_products)    { create_list(:product, 4, taxons: [related_taxon]) }
-  given!(:not_related_product) { create(:product, taxons: [not_related_taxon]) }
 
   given!(:taxonomy)         { create(:taxonomy) }
-  given(:related_taxon)     { create(:taxon) }
-  given(:not_related_taxon) { create(:taxon) }
+  given(:related_taxon)     { create(:taxon, parent: taxonomy.root, taxonomy: taxonomy) }
+  given(:not_related_taxon) { create(:taxon, parent: taxonomy.root, taxonomy: taxonomy) }
 
   scenario "render show_page and index_page" do
     visit potepan_product_path product.id
@@ -33,11 +34,12 @@ RSpec.feature "Visiting Product Page", type: :feature do
 
   scenario "seeing related_products" do
     visit potepan_product_path product.id
-    # 同じカテゴリーの関連商品だけ表示されているか
+    # 同じカテゴリーの関連商品が４つだけ表示されているか
     within ".productsContent" do
       related_products.each do |related_product|
         expect(page).to have_content related_product.name
       end
+      expect(page).not_to have_content related_product_5.name
       expect(page).not_to have_content not_related_product.name
       expect(page).not_to have_content product.name
     end
