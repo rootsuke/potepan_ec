bf11b5cd31f84f550a774b519b98fad6cdca7fc8
root_suke <rootsuke@gmail.com>
2018-12-01 15:59:45 +0900

    テストデータが全て事前に作成されるように修正

--- spec/models/spree/product_decorator_spec.rb
+++ spec/models/spree/product_decorator_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe Spree::Product, type: :model do
   subject { Spree::Product.related_products_of(product) }
 
-  let(:product)             { create(:product, taxons: [related_taxon]) }
-  let(:related_product)     { create(:product, taxons: [related_taxon]) }
-  let(:not_related_product) { create(:product, taxons: [not_related_taxon]) }
+  let(:product)              { create(:product, taxons: [related_taxon]) }
+  let!(:related_product)     { create(:product, taxons: [related_taxon]) }
+  let!(:not_related_product) { create(:product, taxons: [not_related_taxon]) }
 
   let(:taxonomy)          { create(:taxonomy) }
   let(:related_taxon)     { create(:taxon, parent: taxonomy.root, taxonomy: taxonomy) }
