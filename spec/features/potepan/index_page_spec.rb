a47ee055109d27e203a3a5f3d07c892706440034
root_suke <rootsuke@gmail.com>
2018-12-02 16:41:35 +0900

    新着商品が正しく表示されているかについてのfeature specを追加

--- spec/features/potepan/index_page_spec.rb
+++ spec/features/potepan/index_page_spec.rb
@@ -1,12 +1,32 @@
 require 'rails_helper'
 
 RSpec.feature "Visiting Index Page", type: :feature do
+  let!(:product_oldest)      { create(:product, available_on: 2.days.ago) }
+  let!(:product_old)         { create(:product, available_on: 1.day.ago) }
+  let!(:product_new)         { create(:product, available_on: DateTime.now) }
+  let!(:not_arrival_product) { create(:product, available_on: 2.days.since) }
+  let(:new_arrival_products) { [product_oldest, product_old, product_new] }
+
   let!(:taxonomy) { create(:taxonomy, name: "Category") }
 
-  scenario "render successfully" do
+  background do
     visit potepan_root_path
+  end
+
+  scenario "render successfully" do
     click_link "Home"
     expect(page).to have_title "BIGBAG Store"
     expect(current_path).to eq potepan_index_path
+  end
+
+  feature "New_arrival_products" do
+    scenario "seeing in page" do
+      within "#new_arrival" do
+        new_arrival_products.each do |new_arrival_product|
+          expect(page).to have_content new_arrival_product.name
+        end
+        expect(page).not_to have_content not_arrival_product.name
+      end
+    end
   end
 end
