ed7e19eae10d47b5d657f5582d620e2346724215
root_suke <rootsuke@gmail.com>
2018-12-04 22:44:05 +0900

    関連商品を取得するクエリを修正
    RAND()はパフォーマンスが悪くなる可能性があるため、関連商品のidをランダムに取得しwhereで検索するように修正

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,10 +1,8 @@
 class Potepan::ProductsController < ApplicationController
-  NUMBER_OF_RELATED_PRODUCTS = 4
-
   def show
     @product = Spree::Product.find(params[:id])
     @properties = @product.product_properties.includes(:property)
     @shipping_methods = @product.shipping_category.shipping_methods
-    @related_products = Spree::Product.related_products_of(@product).limit(NUMBER_OF_RELATED_PRODUCTS)
+    @related_products = Spree::Product.related_products_of(@product)
   end
 end
--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,7 +1,10 @@
 Spree::Product.class_eval do
+  NUMBER_OF_RELATED_PRODUCTS = 4
+
   scope :related_products_of, -> (product) do
-    joins(:classifications).includes(master: [:images, :default_price]).
-      where(spree_products_taxons: { taxon_id: product.taxons }).
-      where.not(id: product.id).order(Arel.sql("RAND()")).distinct
+    related_products_ids =
+      joins(:classifications).where(spree_products_taxons: { taxon_id: product.taxons }).
+        where.not(id: product.id).distinct.pluck(:id).sample(NUMBER_OF_RELATED_PRODUCTS)
+    includes(master: [:images, :default_price]).where(id: related_products_ids)
   end
 end
