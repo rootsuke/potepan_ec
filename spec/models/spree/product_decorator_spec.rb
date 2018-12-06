72d6b71a7eead05715f5b0baaf98a585ebdd9e01
root_suke <rootsuke@gmail.com>
2018-12-07 00:26:39 +0900

    related_products_idsに想定外の値が入っていないかテストを追加

--- spec/models/spree/product_decorator_spec.rb
+++ spec/models/spree/product_decorator_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Spree::Product, type: :model do
-  subject { Spree::Product.related_products_of(product) }
-
   let(:product)              { create(:product, taxons: [related_taxon]) }
   let!(:related_products)    { create_list(:product, 4, taxons: [related_taxon]) }
   let!(:not_related_product) { create(:product, taxons: [not_related_taxon]) }
@@ -11,23 +9,35 @@ RSpec.describe Spree::Product, type: :model do
   let(:related_taxon)     { create(:taxon, parent: taxonomy.root, taxonomy: taxonomy) }
   let(:not_related_taxon) { create(:taxon, parent: taxonomy.root, taxonomy: taxonomy) }
 
-  it { is_expected.not_to contain_exactly not_related_product }
+  describe "Scope: related_products_of" do
+    subject { Spree::Product.related_products_of(product) }
 
-  it { is_expected.not_to contain_exactly product }
+    it { is_expected.not_to include not_related_product }
 
-  context "related_taxon has 4 products" do
-    it { is_expected.to match_array related_products }
+    it { is_expected.not_to include product }
 
-    it "the number of related_products is 4" do
-      expect(subject.count).to eq 4
+    context "related_taxon has 4 products" do
+      it { is_expected.to match_array related_products }
+
+      it "the number of related_products is 4" do
+        expect(subject.count).to eq 4
+      end
     end
-  end
 
-  context "related_taxon has 5 products" do
-    let!(:related_products_5) { create(:product, taxons: [related_taxon]) }
+    context "related_taxon has 5 products" do
+      let!(:related_products_5) { create(:product, taxons: [related_taxon]) }
 
-    it "the number of related_products is 4" do
-      expect(subject.count).to eq 4
+      it "the number of related_products is 4" do
+        expect(subject.count).to eq 4
+      end
+    end
+
+    describe "related_products_ids" do
+      subject { product.related_products_ids }
+
+      it { is_expected.not_to include not_related_product.id }
+
+      it { is_expected.not_to include product.id }
     end
   end
 end
