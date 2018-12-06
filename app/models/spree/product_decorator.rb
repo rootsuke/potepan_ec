264f62d926eaab4954c26e44ee725129ed1a929b
root_suke <rootsuke@gmail.com>
2018-12-06 23:26:05 +0900

    scope内の変数をメソッドに移譲

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,10 +1,12 @@
 Spree::Product.class_eval do
   NUMBER_OF_RELATED_PRODUCTS = 4
 
+  def related_products_ids
+    Spree::Product.joins(:classifications).where(spree_products_taxons: { taxon_id: taxons }).
+      where.not(id: id).distinct.pluck(:id).sample(NUMBER_OF_RELATED_PRODUCTS)
+  end
+
   scope :related_products_of, -> (product) do
-    related_products_ids =
-      joins(:classifications).where(spree_products_taxons: { taxon_id: product.taxons }).
-        where.not(id: product.id).distinct.pluck(:id).sample(NUMBER_OF_RELATED_PRODUCTS)
-    includes(master: [:images, :default_price]).where(id: related_products_ids)
+    includes(master: [:images, :default_price]).where(id: product.related_products_ids)
   end
 end
