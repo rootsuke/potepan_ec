0d7e778cdbce855986753641f2e2e856ab745b22
root_suke <rootsuke@gmail.com>
2019-01-29 16:40:09 +0900

    remove wrong group_by query

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -4,6 +4,6 @@ Spree::Variant.class_eval do
   end
 
   scope :fetch_variants_of, -> (products_ids, option_value) do
-    joins(:option_values).where(product_id: products_ids, spree_option_values: { presentation: option_value }).group(:product_id)
+    joins(:option_values).where(product_id: products_ids, spree_option_values: { presentation: option_value })
   end
 end
